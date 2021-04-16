97819f32dd0e8fbcca2d3ad394e47919b133cc11
Shahin <mdshahinmije96@gmail.com>
2021-04-16 19:07:51 +0600

    api generate

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+    Route::get('/products', 'ProductController@index');
+    Route::get('/products/{product}', 'ProductController@show');
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+    Route::get('/cart', 'CartController@index');
+    Route::post('/cart', 'CartController@store');
+    Route::delete('/cart/{productId}', 'CartController@destroy');
+    Route::delete('/cart', 'CartController@destroyAll');
